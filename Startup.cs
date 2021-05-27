32c8d4aaf4b0cef18265c616dac7fcea41ff72cf
Vincent Nwonah <vnwonah@outlook.com>
2021-05-27 17:10:37 +0100

    csproj update

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace DOTVVMAWSDeployDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            //loggerFactory.AddConsole();
 
             // use DotVVM
             var dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(env.ContentRootPath);
